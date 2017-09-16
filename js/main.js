d6210b6ec66ca5ece98fb5fe28d36e9ec7ecb0ec
Gniewogniew <jakubbajer83@gmail.com>
2017-09-16 13:36:28 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 function main() {
     $(".burger-menu").click(function() {
-        $(".sidebar").toggleClass("open");
-        $(".burger-menu").toggleClass('open');
+        $(".sidebar,.burger-menu").toggleClass("open");
     });
 }
 $(main)
